220b8aae27dddfccee876ea390dbb192ecfad9c0
Chandler <74741436+scottcd@users.noreply.github.com>
2021-09-16 17:56:45 -0400

    Add files via upload

---
+++ InstructionSet/ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using InstructionLibrary;
+
+namespace ConsoleUI {
+    class Program {
+        static void Main(string[] args) {
+            /*
+             1. feed input
+             2. decode hex -> instruction
+             3. print instructions
+             */
+            
+            Console.WriteLine(InstructionTable.add);
+        }
+    }
+}
---
+++ InstructionSet/InstructionLibrary/InstructionTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstructionLibrary {
+    public enum InstructionTable {
+        add =   0b000,
+        sub =   0b0001,
+        and =   0b0010,
+        or =    0b0011,
+        nor =   0b0100,
+        xor =   0b1110,
+
+
+
+        R_Type = add | sub | and | or | nor | xor,
+        I_Type = add | sub,
+        J_Type = add | sub
+    }
+}
