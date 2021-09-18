747275d80168d76fe7badcfa07eed18264586a01
John-Pil <89737096+John-Pil@users.noreply.github.com>
2021-09-18 16:40:19 -0400

    Add files via upload

---
+++ InstructionSet/ISAForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace WinFormsUI {
+    partial class ISAForm {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.stateBox = new System.Windows.Forms.RichTextBox();
+            this.CompileButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.outputBox = new System.Windows.Forms.RichTextBox();
+            this.inputBox = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // stateBox
+            // 
+            this.stateBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.stateBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.stateBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.stateBox.DetectUrls = false;
+            this.stateBox.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.stateBox.Location = new System.Drawing.Point(1229, 12);
+            this.stateBox.Name = "stateBox";
+            this.stateBox.ReadOnly = true;
+            this.stateBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.stateBox.Size = new System.Drawing.Size(198, 769);
+            this.stateBox.TabIndex = 3;
+            this.stateBox.Text = "A0 5F 3C B2 A0 5F 3C B2 3C B2";
+            // 
+            // CompileButton
+            // 
+            this.CompileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CompileButton.BackColor = System.Drawing.SystemColors.Menu;
+            this.CompileButton.Location = new System.Drawing.Point(1229, 787);
+            this.CompileButton.Name = "CompileButton";
+            this.CompileButton.Size = new System.Drawing.Size(198, 32);
+            this.CompileButton.TabIndex = 1;
+            this.CompileButton.Text = "Compile";
+            this.CompileButton.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.outputBox);
+            this.panel1.Controls.Add(this.inputBox);
+            this.panel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1211, 807);
+            this.panel1.TabIndex = 2;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.outputBox.DetectUrls = false;
+            this.outputBox.Location = new System.Drawing.Point(0, 400);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(1211, 407);
+            this.outputBox.TabIndex = 2;
+            this.outputBox.Text = "";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputBox.DetectUrls = false;
+            this.inputBox.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.inputBox.Location = new System.Drawing.Point(0, 0);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(1211, 394);
+            this.inputBox.TabIndex = 1;
+            this.inputBox.Text = "";
+            // 
+            // ISAForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1439, 831);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.CompileButton);
+            this.Controls.Add(this.stateBox);
+            this.MaximumSize = new System.Drawing.Size(1455, 870);
+            this.MinimumSize = new System.Drawing.Size(1455, 870);
+            this.Name = "ISAForm";
+            this.Text = "ISA UI";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox stateBox;
+        private System.Windows.Forms.Button CompileButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox outputBox;
+        private System.Windows.Forms.RichTextBox inputBox;
+    }
+}
+
